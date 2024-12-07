7ef46227dbed7c055ba2e93ba6c4d0af9dacd23b
Daniel <ee23dp003@iitdh.ac.in>
2024-12-07 10:34:42 +0530

    Initial commit

---
+++ LED_Toggle_CPU_Timer/LED_Toggle_CPU_Timer.c
@@ -0,0 +1,115 @@
+//#############################################################################
+//
+// FILE:    gpio_ex2_toggle.c
+//
+// TITLE:   Device GPIO Toggle
+//
+//! \addtogroup driver_example_list
+//! <h1> Device GPIO Toggle </h1>
+//!
+//! Configures the device GPIO through the sysconfig file. The GPIO pin is 
+//! toggled in the infinite loop.
+//
+//#############################################################################
+//
+// 
+// $Copyright:
+// Copyright (C) 2013-2024 Texas Instruments Incorporated - http://www.ti.com/
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+// 
+//   Redistributions of source code must retain the above copyright 
+//   notice, this list of conditions and the following disclaimer.
+// 
+//   Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the 
+//   documentation and/or other materials provided with the   
+//   distribution.
+// 
+//   Neither the name of Texas Instruments Incorporated nor the names of
+//   its contributors may be used to endorse or promote products derived
+//   from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+// $
+//#############################################################################
+#include "driverlib.h"
+#include "device.h"
+#include "board.h"
+
+//*****************************************************************************
+//
+// Main Function
+//
+//*****************************************************************************
+void main(void)
+{
+    //
+    // Initialize device clock and peripherals
+    //
+    Device_init();
+
+    //
+    // Initialize PIE and clear PIE registers. Disables CPU interrupts.
+    //
+    Interrupt_initModule();
+
+    //
+    // Initialize the PIE vector table with pointers to the shell ISR.
+    // This will populate the entire table, even if the interrupt is not used.
+    //
+    Interrupt_initVectorTable();
+
+    //
+    // Initialize all peripherals and configurations using board.c functions
+    //
+    Board_init();
+
+    //
+    // Enable Global Interrupt (INTM) and Real-time interrupt (DBGM)
+    //
+    EINT;  // Enable Global interrupt INTM
+    ERTM;  // Enable Global real-time interrupt DBGM
+
+    //
+    // Infinite loop
+    //
+    while(1)
+    {
+        //
+        // Insert low-power mode or idle code here if required
+        //
+        asm(" NOP"); // Placeholder instruction
+    }
+}
+
+//*****************************************************************************
+//
+// CPU Timer 0 ISR
+//
+//*****************************************************************************
+__interrupt void INT_myCPUTIMER0_ISR(void)
+{
+    //
+    // Toggle the onboard red LED (GPIO31)
+    //
+    GPIO_togglePin(BOARD_GPIO31);
+
+    //
+    // Clear the interrupt flag and acknowledge the interrupt group
+    //
+    CPUTimer_clearOverflowFlag(myCPUTIMER0_BASE);
+    Interrupt_clearACKGroup(INT_myCPUTIMER0_INTERRUPT_ACK_GROUP);
+}
